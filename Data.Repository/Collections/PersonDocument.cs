f7f206dcbf0bc8b2588d9b5c0f15fabcf64812ce
Sérgio Sousa <sfps23@hotmail.com>
2020-11-17 23:36:12 +0000

    add person mappers for mongo objects
    Signed-off-by: Sérgio Sousa <sfps23@hotmail.com>

---
+++ Data.Repository/Collections/PersonDocument.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Humanis.Data.Repository.Collections
+{
+    [BsonIgnoreExtraElements]
+    public class PersonDocument
+    {
+        [BsonElement("identifier")]
+        public string Identifier { get; set; }
+
+        [BsonElement("first_name")]
+        public string FirstName { get; set; }
+
+        [BsonElement("last_name")]
+        public string LastName { get; set; }
+    }
+}
---
+++ Data.Repository/Mappers/PersonDocumentMapper.cs
@@ -0,0 +1,38 @@
+﻿using Humanis.Data.Repository.Collections;
+using Humanis.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Humanis.Data.Repository.Mappers
+{
+    public static class PersonDocumentMapper
+    {
+        public static IEnumerable<Person> ToModel(this IEnumerable<PersonDocument> source)
+        {
+            if (source == null)
+            {
+                return null;
+            }
+
+            return source.Select(x => x.ToModel());
+        }
+
+
+        public static Person ToModel(this PersonDocument source)
+        {
+            if (source == null)
+            {
+                return null;
+            }
+
+            return new Person
+            {
+                Id = new Guid(source.Identifier),
+                FirstName = source.FirstName,
+                LastName = source.LastName
+            };
+        }
+    }
+}
